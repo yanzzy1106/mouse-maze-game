e745b10029b7ff85e1b11a522a2b9c796585c7f0
yanzzy1106 <1353029874@qq.com>
2025-01-08 22:43:40 +0800

    Update game.js

--- game.js
+++ game.js
@@ -18,9 +18,9 @@ var game = new Phaser.Game(config);
 
 function preload() {
     // 预加载图片资源
-    this.load.image('mouse', 'assets/mouse.png');
-    this.load.image('cheese', 'assets/cheese.png');
-    this.load.image('wall', 'assets/wall.png');
+    this.load.image('mouse', '/Users/ziyueyan/Desktop/assets/mouse.png');
+    this.load.image('cheese', '/Users/ziyueyan/Desktop/assets/cheeses.png');
+    this.load.image('wall', '/Users/ziyueyan/Desktop/assets/wall.png');
 }
 
 function create() {
